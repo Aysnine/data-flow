f7f914c97367e2ee2ae24758d986141a9ce36f52
CNine <cnine229@gmail.com>
2024-11-11 22:58:36 +0800

    chore: add docker compose

--- migrations/clickhouse/001__init_data_lineage_table.up.sql
+++ migrations/clickhouse/001__init_data_lineage_table.up.sql
@@ -1,6 +1,17 @@
 CREATE TABLE IF NOT EXISTS data_lineage (
-  from_id String,
+  to_table LowCardinality(String),
   to_id String,
+  from_table LowCardinality(String),
+  from_ids Array(String),
+  to_metric_keys Array(String),
   created_at DateTime DEFAULT now()
-) ENGINE = ReplacingMergeTree()
-ORDER BY (from_id, to_id) COMMENT 'data lineage';
+) ENGINE = MergeTree()
+PARTITION BY toYYYYMM(created_at)
+ORDER BY (to_table, to_id)
+SETTINGS index_granularity = 8192,
+         min_bytes_for_wide_part = 0,
+         write_final_mark = 0
+
+INDEX idx_metric_keys to_metric_keys TYPE set(0) GRANULARITY 1
+
+COMMENT 'data lineage';
